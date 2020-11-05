62475d21c78191e9f8041499281d73d85fdf0c03
TravelFiend <mikegrace274@gmail.com>
2020-11-05 14:34:38 -0500

    fetch and save methods up in there

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log('hi punk');
+import { User } from './models/User';
+
+const user = new User({ name: 'It\'s A\' Me', age: 25 });
+
+user.save();
---
+++ src/models/User.ts
@@ -0,0 +1,53 @@
+import Axios, { AxiosResponse } from "axios";
+
+interface UserProps {
+  id?: number,
+  name?: string,
+  age?: number
+};
+
+type Callback = () => void;
+
+export class User {
+  events: { [key: string]: Callback[] } = {};
+
+  constructor(private data: UserProps){};
+
+  get(propName: string): (string | number) {
+    return this.data[propName];
+  };
+
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
+  };
+
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  };
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    if(!handlers || !handlers.length){ return };
+
+    handlers.forEach(callback => callback());
+  }
+
+  fetch(): void {
+    Axios.get(`http://localhost:3000/users/${this.get('id')}`)
+      .then((res: AxiosResponse): void => {
+        this.set(res.data);
+      })
+  }
+
+  save(): void {
+    const id = this.get('id');
+    if(id){
+      Axios.put(`http://localhost:3000/users/${id}`, this.data);
+    } else {
+      Axios.post('http://localhost:3000/users', this.data);
+    }
+  }
+}
